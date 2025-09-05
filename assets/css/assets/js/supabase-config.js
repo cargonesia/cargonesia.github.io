796b8f607c55f85883f0ca28e8a0d4d308fb21ad
cargonesia <rofikgrahatrans@gmail.com>
2025-09-06 01:46:06 +0700

    Delete assets/css/assets directory

--- assets/css/assets/js/supabase-config.js
+++
@@ -1,3 +0,0 @@
-const SUPABASE_URL = "https://hbhebwlnausycviijkjl.supabase.co"; // ganti dengan project kamu
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...";  // anon key
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
